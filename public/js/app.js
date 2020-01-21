0d8a3948edac02290947b777710970cd94d7aa2f
Vipin-1999 <vipinrbharadwaj@gmail.com>
2020-01-21 20:54:27 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,33 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const message = document.querySelector('#message')
+const messageLocation = document.querySelector('#location')
+const messageForecast = document.querySelector('#forecast')
+const messageLatitude = document.querySelector('#latitude')
+const messageLongitude = document.querySelector('#longitude')
+const messageCoordinates = document.querySelector('#coordinates')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    var location = search.value
+
+    message.textContent = 'Showing forecast..'
+    messageLocation.textContent = ''
+    messageForecast.textContent = ''
+    messageLatitude.textContent = ''
+    messageLongitude.textContent = ''
+
+    fetch('http://localhost:3000/weather?address='+location).then((response) => {
+        response.json().then((data) =>  {
+            if(data.error)  
+                messageOne.textContent = data.error
+            else    {
+                messageLocation.textContent = 'Location : ' + data.Location
+                messageForecast.textContent = 'Forecast : ' + data.Forecast
+                messageCoordinates.textContent = 'Coordinates are :'
+                messageLatitude.textContent = 'Latitude : ' + data.Coordinates.Latitude
+                messageLongitude.textContent = 'Longitude : ' + data.Coordinates.Longitude
+            }
+        })
+    })
+})
